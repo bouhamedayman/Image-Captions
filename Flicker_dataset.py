05535f57e4e5c5650887e71b0a4ae97a8a64b79f
aymen.bouhamed <aymen.bouhamed@euranova.eu>
2021-09-18 23:41:56 +0100

    add Flicker_dataset class

---
+++ Flicker_dataset.py
@@ -0,0 +1,45 @@
+from Vocabulary import Vocabulary
+import os
+from torch.utils.data import Dataset
+from PIL import Image
+from torchvision import transforms
+import pandas as pd
+import torch
+import pickle
+""" in Flicker_dataset we are going to make the data callable so we 
+    can get the image tensor transformed and get the numericalzed 
+    version of the  text  """
+class Flicker_dataset(Dataset):
+    def __init__(self,root_dir,captions_file,transform=None,freq_threshold=5,build_vocab=True):
+        self.root_dir=root_dir  #the directory of the images 
+        self.freq_threshold=freq_threshold
+        self.df=pd.read_csv(captions_file)
+        self.transform=transform #image transformers
+        self.captions=self.df.caption
+        self.image_id=self.df.image
+        if build_vocab: #if we build the vocab for the first time 
+            self.vocabulary=Vocabulary(self.freq_threshold)
+            self.vocabulary.build_vocabulary(self.captions.to_list())
+        else: #if we load the vocab file 
+            with open('vocab.pkl','rb') as vocab:
+                self.vocabulary=pickle.load(vocab)
+    def __len__(self):#length of the data 
+        return(self.df.shape[0])
+    def __getitem__(self, index) :#get the transformed image and the numericalized text
+
+        caption=self.captions[index]
+        image_id=self.image_id[index]
+        image=Image.open(os.path.join(self.root_dir,str(image_id))).convert("RGB")
+        if self.transform is not None:
+            image=self.transform(image)
+        numericalized_caption=[self.vocabulary.string_to_index["<SOS>"]]
+        numericalized_caption+=self.vocabulary.numericalize(caption)
+        numericalized_caption+=[self.vocabulary.string_to_index["<EOS>"]]
+        return image    ,   torch.tensor(numericalized_caption)
+"""root_dir=os.path.join(os.getcwd(),'data/Flicker8k_Dataset')
+captions_file=os.path.join(os.getcwd(),"data/captions.txt")
+transform=transforms.ToTensor()
+object=Flicker_dataset(root_dir=root_dir,captions_file=captions_file,
+transform=transform,build_vocab= False)
+print(object[0])
+"""
--- Vocabulary.py
+++ Vocabulary.py
@@ -1,6 +1,11 @@
 import spacy
 spacy_eng=spacy.load("en")
+import pickle
+#we create Vocabulary dataset so it can process the 
+#text data and return a numerical representation of
+#the sentences 
 class Vocabulary:
+    
     def __init__(self,freq_threshold=5) :#if a word is repeated more then the 
                                         #Threshold we will assign UNK to it
         self.index_to_string={0:"<PAD>",1:"<SOS>",2:"<EOS>",3:"<UNK>"}#idx to string dictionnary
@@ -10,10 +15,12 @@ class Vocabulary:
     def __len__(self):
         return (len(self.index_to_string))
     def tokenizer_eng(self,text):#tokenizing the sentence
-        return [tok.text.lower for tok in spacy_eng(text)]
+        return [tok.text.lower() for tok in spacy_eng(text)]
     def build_vocabulary(self,sentences_list):
         frequencies={}#to count words frequnecies
         idx=4
+        print('building vocabulary please wait ... \n')
+    
         for sentence in sentences_list:
             for word in self.tokenizer_eng(sentence):
                 if word not in frequencies.keys():
@@ -25,6 +32,13 @@ class Vocabulary:
                     self.string_to_index[word]=idx
                     self.index_to_string[idx]=word
                     idx+=1
+                    if idx%1000==0:
+                        print('building vocabulary please wait ... \n')
+        
+        with open('vocab.pkl','wb') as vocab:
+            pickle.dump(self,vocab,pickle.HIGHEST_PROTOCOL)
+
+        print("Vocabulary built  ")
     def numericalize(self,text):#return a numericalized text 
         tokenized_text=self.tokenizer_eng(text)
         num_text=[]
